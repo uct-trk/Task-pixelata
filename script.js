cbe05e79b9595d55524b9dc514e527e00ddd0b69
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-06-21 12:54:17 +0300

    responsive güncelleme

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const navbar = document.querySelector(".hamburger-menu");
 const form = document.querySelector(".form-section");
 const footer = document.querySelector(".container");
 const slogan = document.querySelector(".page-info");
-const box = document.querySelector(".box2")
+const box = document.querySelector(".box2");
+const btn = document.querySelector(".header-right-menu_button");
+
+
 
 // hamburger menu acılıp kapanıyor
 menuIcon.addEventListener("click", toggleMenu);
@@ -15,4 +18,7 @@ function toggleMenu(){
     footer.classList.toggle("container-removed")
     slogan.classList.toggle("page-info-removed")
     box.classList.toggle("box2-add")
+    btn.classList.toggle("header-right-menu_button-active")
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("active");
 }
